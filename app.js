c06931142a003c8ef0d4bff06a0e31044fd9eaf8
rasitdeveloper <frasitkilic@gmail.com>
2022-02-27 17:04:22 +0300

    Create Project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    const photo = {
+        id: 1,
+        name: "Photo Name",
+        description: "Photo description"
+    }
+    res.send(photo);
+})
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı..`);
+})
